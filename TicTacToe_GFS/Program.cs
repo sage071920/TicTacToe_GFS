dc01055ac42a16069968ef069dd2abb302df492f
sage071920 <samuel.henze@gmx.de>
2025-11-07 23:37:39 +0100

    Refactor program structure and enhance keyboard input handling for Tic Tac Toe

--- TicTacToe_GFS/Program.cs
+++ TicTacToe_GFS/Program.cs
@@ -1,30 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 
 namespace TicTacToe_GFS
 {
-    static class Program
+    internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        
-        
-        
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
-            Steuerung steuerung = new Steuerung();
-            Oberfläche dieGui = new Oberfläche(steuerung);
-            // Erstelle Steuerung und starte deren GUI
-            Application.Run(dieGui);
+
+            Steuerung steuerung = new Steuerung();  // Startet alles
+            Application.Run(steuerung._dieGui);     // Zeigt das Formular
         }
     }
 }
